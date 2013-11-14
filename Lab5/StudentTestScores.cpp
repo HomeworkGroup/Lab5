ff03726b0ff7f9187be113a89c0ed6359f73d683
MakiseKurisu <saberconer@gmail.com>
2013-11-13 21:19:28 -0600

    Formatting

--- Lab5/StudentTestScores.cpp
+++ Lab5/StudentTestScores.cpp
@@ -5,139 +5,139 @@ using namespace std;
 
 StudentTestScores::StudentTestScores()
 {
-	studentName = "";
-	numTestScores = 0;
-	testScores = NULL;
+    studentName = "";
+    numTestScores = 0;
+    testScores = NULL;
 }
 
 StudentTestScores::StudentTestScores(string name, int numscores)
 {
-	studentName = name;
-	numTestScores = numscores;
+    studentName = name;
+    numTestScores = numscores;
 
-	if(!numscores)
-		testScores = NULL;
-	else
-		testScores = new double[numTestScores];
+    if (!numscores)
+        testScores = NULL;
+    else
+        testScores = new double[numTestScores];
 
 }
 
 StudentTestScores::~StudentTestScores()
 {
-	delete [] testScores;
+    delete[] testScores;
 }
 
 StudentTestScores::StudentTestScores(const StudentTestScores &other)
 {
-	studentName = other.studentName;
-	numTestScores = other.numTestScores;
-	testScores = new double[numTestScores];
-	for(int i=0; i<numTestScores; i++)
-		testScores[i] = other.testScores[i];
+    studentName = other.studentName;
+    numTestScores = other.numTestScores;
+    testScores = new double[numTestScores];
+    for (int i = 0; i<numTestScores; i++)
+        testScores[i] = other.testScores[i];
 }
 
 void StudentTestScores::displayTestScores()
 {
-	cout<<studentName<<'\t';
-	cout<<numTestScores;
-	for(int i=0; i<numTestScores; i++)
-		cout<<'\t'<<testScores[i];
-	cout<<'\t'<<getAverage()<<'\t';
-	cout<<getLetterGrade();
-	cout<<endl;
+    cout << studentName << '\t';
+    cout << numTestScores;
+    for (int i = 0; i<numTestScores; i++)
+        cout << '\t' << testScores[i];
+    cout << '\t' << getAverage() << '\t';
+    cout << getLetterGrade();
+    cout << endl;
 }
 
 void StudentTestScores::setName(string name)
 {
-	studentName = name;
+    studentName = name;
 }
 
 void StudentTestScores::setOneTestScore(int index, double score)
 {
-	testScores[index] = score;
+    testScores[index] = score;
 }
 
 double StudentTestScores::getAverage()
 {
-	double sum=0;
+    double sum = 0;
 
-	for(int i=0; i<numTestScores; i++)
-	{
-		sum+=testScores[i];
-	}
+    for (int i = 0; i<numTestScores; i++)
+    {
+        sum += testScores[i];
+    }
 
-	return (sum/numTestScores);
+    return (sum / numTestScores);
 }
 
 string StudentTestScores::getLetterGrade()
 {
-	if (!testScores)
-		return "U";
+    if (!testScores)
+        return "U";
 
-	double avg=getAverage();
+    double avg = getAverage();
 
-	if(avg>=90.0)
-		return "A";
-	else if(avg>=80.0 && avg<90.0)
-		return "B";
-	else if(avg>=70.0 && avg<80.0)
-		return "C";
-	else if(avg>=60.0 && avg<70.0)
-		return "D";
-	else if(avg<60.0)
-		return "F";
+    if (avg >= 90.0)
+        return "A";
+    else if (avg >= 80.0 && avg<90.0)
+        return "B";
+    else if (avg >= 70.0 && avg<80.0)
+        return "C";
+    else if (avg >= 60.0 && avg<70.0)
+        return "D";
+    else if (avg<60.0)
+        return "F";
 }
 
 string StudentTestScores::getName()
 {
-	return studentName;
+    return studentName;
 }
 
 double StudentTestScores::getOneTestScore(int index)
 {
-	return testScores[index];
+    return testScores[index];
 }
 
 StudentTestScores StudentTestScores::operator=(const StudentTestScores &other)
 {
-	delete [] testScores;
-	studentName = other.studentName;
-	numTestScores = other.numTestScores;
-	testScores = new double[numTestScores];
-	for(int i=0; i<numTestScores; i++)
-		testScores[i] = other.testScores[i];
+    delete[] testScores;
+    studentName = other.studentName;
+    numTestScores = other.numTestScores;
+    testScores = new double[numTestScores];
+    for (int i = 0; i<numTestScores; i++)
+        testScores[i] = other.testScores[i];
 
-	return other;
+    return other;
 }
 
 ostream& operator<<(ostream& ostr, const StudentTestScores& obj)
 {
-	StudentTestScores dummy = obj;
+    StudentTestScores dummy = obj;
 
-	ostr<<dummy.studentName<<'\t';
-	ostr<<dummy.numTestScores;
-	for(int i=0; i<dummy.numTestScores; i++)
-		ostr<<'\t'<<dummy.testScores[i];
-	ostr<<'\t'<<dummy.getAverage()<<'\t';
-	ostr<<dummy.getLetterGrade();
-	ostr<<endl;
+    ostr << dummy.studentName << '\t';
+    ostr << dummy.numTestScores;
+    for (int i = 0; i<dummy.numTestScores; i++)
+        ostr << '\t' << dummy.testScores[i];
+    ostr << '\t' << dummy.getAverage() << '\t';
+    ostr << dummy.getLetterGrade();
+    ostr << endl;
 
-	return ostr;
+    return ostr;
 }
 
 istream& operator>>(istream& istr, StudentTestScores& obj)
 {
-	cout<<"Enter student name: ";
-	getline(istr, obj.studentName);
-	cout<<"Enter number of scores: ";
-	istr>>obj.numTestScores;
+    cout << "Enter student name: ";
+    getline(istr, obj.studentName);
+    cout << "Enter number of scores: ";
+    istr >> obj.numTestScores;
 
-	obj.testScores = new double[obj.numTestScores];
-	for(int i=0; i<obj.numTestScores; i++)
-	{
-		cout<<"Enter test score "<<(i+1)<<": ";
-		istr>>obj.testScores[i];
-	}
+    obj.testScores = new double[obj.numTestScores];
+    for (int i = 0; i<obj.numTestScores; i++)
+    {
+        cout << "Enter test score " << (i + 1) << ": ";
+        istr >> obj.testScores[i];
+    }
 
-	return istr;
+    return istr;
 }
--- Lab5/StudentTestScores.h
+++ Lab5/StudentTestScores.h
@@ -10,38 +10,38 @@ class StudentTestScores;
 ostream& operator<<(ostream& ostr, const StudentTestScores &obj);
 istream& operator>>(istream& istr, StudentTestScores &obj);
 
- class StudentTestScores
- {
-    private:
-	string studentName;
-	int numTestScores;
-	double *testScores;
-	
-   public:
-	StudentTestScores(); //New default constructor
-	StudentTestScores(string , int );
-	StudentTestScores(const StudentTestScores &other);      // class copy constructor
-	~StudentTestScores();  				// class destructor
+class StudentTestScores
+{
+private:
+    string studentName;
+    int numTestScores;
+    double *testScores;
+
+public:
+    StudentTestScores(); //New default constructor
+    StudentTestScores(string, int);
+    StudentTestScores(const StudentTestScores &other);      // class copy constructor
+    ~StudentTestScores();  				// class destructor
 
 
-	void displayTestScores();							
-	
-//  *** ADD accessors & mutators HERE
-	
-	void setName(string name);
-	void setOneTestScore(int index, double score);
+    void displayTestScores();
 
-	double getAverage();
-	string getLetterGrade();
-	string getName();
-	double getOneTestScore(int index);
+    //  *** ADD accessors & mutators HERE
 
-// overload assignment operator HERE
-	StudentTestScores operator=(const StudentTestScores& other);
+    void setName(string name);
+    void setOneTestScore(int index, double score);
 
-// *** Stream I/O operations here
-	friend ostream& operator<<(ostream& ostr, const StudentTestScores &obj);
-	friend istream& operator>>(istream& istr, StudentTestScores &obj);
+    double getAverage();
+    string getLetterGrade();
+    string getName();
+    double getOneTestScore(int index);
+
+    // overload assignment operator HERE
+    StudentTestScores operator=(const StudentTestScores& other);
+
+    // *** Stream I/O operations here
+    friend ostream& operator<<(ostream& ostr, const StudentTestScores &obj);
+    friend istream& operator>>(istream& istr, StudentTestScores &obj);
 };
 
 #endif 
